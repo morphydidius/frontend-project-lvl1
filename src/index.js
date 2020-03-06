d01989e04811e726a46c81c53d8d506e948b663b
mdidius <morphydidius@gmail.com>
2020-03-07 01:47:56 +0300

    refresh

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
 const printWords = () => {
 	const actual = readlineSync.question('May I have your name? ');
+	// console.log(readlineSync);
 	console.log(`Hello, ${actual}!`);
+	return actual;
 }
 
 export default printWords;
