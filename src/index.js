fcf4da1980b86f1995d6399de50ce7a6801d902f
a.bystrova <a.bistrova@dev.crmm.ru>
2020-03-11 00:50:13 +0300

    eslint continue

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
 const printWords = () => {
-	const actual = readlineSync.question('May I have your name? ');
-	// console.log(readlineSync);
-	console.log(`Hello, ${actual}!`);
-	return actual;
-}
-
+  const actual = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${actual}!`);
+  return actual;
+};
 export default printWords;
